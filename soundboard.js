cf43fbd5eab5aa85e17956ff1d036412e0c74fde
denardju <j.denard2636@gmail.com>
2022-06-20 00:19:29 -0400

    ..

--- soundboard.js
+++ soundboard.js
@@ -6,3 +6,21 @@ function playAhem(){
 }
 
 btn1.addEventListner('click', playAhem);
+
+var btn2 = document.getElementById('btn2')
+var arrow = document.getElementById('arrow')
+
+function playArrow(){
+    arrow.play();
+}
+
+btn2.addEventListner('click', playArrow);
+
+var btn3 = document.getElementById('btn3')
+var boing = document.getElementById('boing')
+
+function playBoing(){
+    boing.play();
+}
+
+btn3.addEventListner('click', playBoing);
